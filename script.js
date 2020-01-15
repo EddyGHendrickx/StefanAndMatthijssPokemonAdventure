bc94a1f7cb9427348ddd9a6068f987cf40330969
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-15 11:45:20 +0100

    added stuff

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 let nameInput = document.getElementById("nameInput");
 let idInput = document.getElementById("idInput");
 let spriteHtml = document.getElementById("sprite");
-
+let finalInput;
 let spritePreviousEvolutionHtml = document.getElementById("spritePrevious");
 let previousEvolutionSprite;
+document.getElementById("run").addEventListener("click",function () {
+
+
+
+
+if (!(nameInput.value === "" && idInput.value === "")){
+    finalInput = idInput
+}
+else if (!(nameInput.value === "")){
+    finalInput = nameInput
+}
+else if (!(idInput.value === "")){
+    finalInput = idInput
+}
+else {alert("Please do enter something")}
 
 // let previousEvolutionApi =
-
-
+getPokemon();
+});
 async function getPokemon() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+    // use backticks ``
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${finalInput}`);
     let pokemonData = await response.json();
     console.log(pokemonData);
     console.log(pokemonData.id);
     console.log(pokemonData.moves);
     console.log(pokemonData.sprites.front_shiny);
+
     let pokemonId = pokemonData.id;
     let pokemondMoves = pokemonData.moves;
     let pokemonSprite = pokemonData.sprites.front_shiny;
+    document.getElementById("pokemonNameHTML").innerHTML = pokemonData.id;
     spriteHtml.setAttribute("src", pokemonSprite);
 
 
-};
+}
 
 
 async function getEvolution() {
@@ -38,3 +56,4 @@ async function getPreviousEvolution() {
 
 getPokemon();
 getEvolution();
+
